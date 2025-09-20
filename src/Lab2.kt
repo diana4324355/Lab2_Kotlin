8cb479344329c4f6243fddf64840e6e518fc90eb
shcmal-chel <shcmalivan@gmail.com>
2025-09-20 13:19:11 +0300

    changee

--- src/Lab2.kt
+++ src/Lab2.kt
@@ -1,5 +1,120 @@
 fun main() {
-    val number = readln().toInt()
-    println(number)
-    println("jjfvff hfmgf")
+    /*
+    print("Введите первое число: ")
+    val number1 = readln().toInt()
+    print("Введите второе число: ")
+    val number2 = readln().toInt()
+    if (number1 > number2){
+        println("Первое число больше ")
+    }else if (number1 == number2){
+        println("Числа равны:")
+    }else{
+        println("Второе число больше ")
+    }
+    val max = if (number1 > number2) number1 else number2
+    println("Максимальное число: $max")
+    */
+
+
+//    val age = readln().toInt()
+//    val status =  if (age >= 18) "Взрослый" else "Ребенок"
+//    println(status)
+
+//    val numbers = 1..100
+//    val numbers2 = 100 downTo 1 step 5
+//    val symbol = 'a'..'z'
+//    for(i in numbers){
+//        println(i)
+    //}
+//    print("Введите порядковый номер месяца (1-12): ")
+//    val index = readln().toInt()
+//    var month = ""
+//    when (index) {
+//        1 -> {
+//            month = "январь"
+//        }
+//
+//        2 -> {
+//            month = "ффевраль"
+//        }
+//
+//        3 -> {
+//            month = "март"
+//        }
+//
+//        4 -> {
+//            month = "апрель"
+//        }
+//
+//        5 -> {
+//            month = "май"
+//        }
+//
+//        6 -> {
+//            month = "июнь"
+//        }
+//
+//        7 -> {
+//            month = "июль"
+//        }
+//
+//        8 -> {
+//            month = "август"
+//        }
+//
+//        9 -> {
+//            month = "сентябрь"
+//        }
+//
+//        10 -> {
+//            month = "октябрь"
+//        }
+//
+//        11 -> {
+//            month = "ноябрь"
+//        }
+//
+//        12 -> {
+//            month = "декабрь"
+//
+//        }
+//
+//        else -> "отсутствует месяц"
+//    };
+//    if (index in 1..12) {
+//        println("Ваш индекс $index cоответствует месяцу $month")
+//    }
+//    else{
+//        println("Индекс вне диапазона")
+//    }
+
+//    print("Введите ваш уровень: ")
+//    val level = readln().toInt()
+//    print("У вас VIP-статус? (true/false): ")
+//    val hasVIP = readln().toBoolean()
+//    if((level >= 30 && hasVIP) || level >= 50) {
+//        println ("Доступ разрешен! ")
+//
+//    }else {
+//        println ("Доступ запрещен!")
+//    }
+//    print ("Введите ваш уровень: ")
+//    val level = readln().toInt()
+//    print ("У вас есть VIP-статус? (true/false): ")
+//    val hasVIP = readln().toBoolean()
+//
+//    print("Введите ваш уровень")
+//    val level = readln().toInt()
+//    if (level in 1..50){
+//        println("Уровень $level соответствует требованиям! вы можете зайти в данж!")
+//    }
+//    else {
+//        println("Высокий уровень! Вход запрещен!")
+//    }
+//    val deapazon = 'z'..'a'
+//    val dip = 5..10
+//    val dipp = 10..50 step 3
+//    println("$deapazon $dip $dipp")
+
+
 }
